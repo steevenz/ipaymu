5dffc76f104e8fd5baf8bebb600dec5349143875
Steeve Andrian Salim <steevenz@ymail.com>
2018-01-30 12:44:57 +0700

    first commit

---
+++ README.md
@@ -0,0 +1,215 @@
+# RajaOngkir
+[![Latest Stable Version](https://poser.pugx.org/steevenz/ipaymu/v/stable)](https://packagist.org/packages/steevenz/ipaymu) [![Total Downloads](https://poser.pugx.org/steevenz/ipaymu/downloads)](https://packagist.org/packages/steevenz/ipaymu) [![Latest Unstable Version](https://poser.pugx.org/steevenz/ipaymu/v/unstable)](https://packagist.org/packages/steevenz/ipaymu) [![License](https://poser.pugx.org/steevenz/ipaymu/license)](https://packagist.org/packages/steevenz/ipaymu)
+
+Ini adalah [Ipaymu][11] API PHP Class, yang berfungsi untuk melakukan request API [Ipaymu][11].
+
+Instalasi
+---------
+Cara terbaik untuk melakukan instalasi library ini adalah dengan menggunakan [Composer][7]
+```
+composer require steevenz/ipaymu
+```
+
+Penggunaan
+----------
+```php
+use Steevenz\Ipaymu;
+
+/*
+ * --------------------------------------------------------------
+ * Inisiasi Class Ipaymu
+ * --------------------------------------------------------------
+ */
+ // Untuk menggunakan API Ipaymu Account saja.
+ $ipaymu = new Ipaymu();
+ $ipaymu->setApiKey('API_KEY_ANDA');
+ 
+ // Untuk menggunakan API Ipaymu Account dan Webstore.
+ $ipaymu = new Ipaymu([
+    'apiKey' => 'API_KEY_ANDA',
+    
+    // Konfigurasi Url diperlukan untuk melakukan transaksi ke ipaymu
+    'url' => [
+        'return' => 'http://www.domainanda.com/terimakasih.html'
+        'notify' => ' http://www.domainanda.com/notify-ipaymu.php'
+        'cancel' => 'http://www.domainanda.com/batal.html'
+    ]
+ ]);
+ 
+/*
+ * --------------------------------------------------------------
+ * Mendapatkan informasi akun Ipaymu
+ * 
+ * @return array
+ * --------------------------------------------------------------
+ */
+$account = $ipaymu->getAccount();
+
+/*
+ * --------------------------------------------------------------
+ * Cek Saldo Akun
+ * Untuk mengecek jumlah saldo terakhir Anda.
+ *
+ * @return int
+ * --------------------------------------------------------------
+ */
+$balance = $ipaymu->checkAccountBalance();
+
+/*
+ * --------------------------------------------------------------
+ * Cek Status Akun
+ * Untuk mengecek status akun iPaymu.
+ *
+ * @return string
+ * --------------------------------------------------------------
+ */
+$status = $ipaymu->checkAccountStatus();
+
+/*
+ * --------------------------------------------------------------
+ * Cek Transaksi
+ *
+ * @param string $trxId Kode Unik Transaksi.
+ * @return array|bool Returns FALSE if failed.
+ * --------------------------------------------------------------
+ */
+$transaction = $ipaymu->checkTransaction('IDX-1234567890');
+
+/*
+ * --------------------------------------------------------------
+ * Melakukan transaksi pembayaran dengan single produk
+ *
+ * @return array|bool   Returns FALSE if failed or returns array contains
+ *                      Ipaymu transaction Url.
+ * --------------------------------------------------------------
+ */
+$ipaymu->addTransaction([
+   'id' => 'INV-1234567890',
+   'product' => [
+       'name' => 'Shoes'
+       'price' => 10000,
+       'quantity' => 1,
+       'description' => 'Amazing Shoes'
+   ]
+]);
+
+/*
+ * --------------------------------------------------------------
+ * Melakukan transaksi pembayaran PayPal dengan single produk
+ * 
+ * @return array|bool   Returns FALSE if failed or returns array contains
+ *                      Ipaymu transaction Url.
+ * --------------------------------------------------------------
+ */
+$ipaymu->addTransaction([
+   'id' => 'INV-1234567890',
+   'product' => [
+       'name' => 'Shoes'
+       'price' => 10000,
+       'price_usd' => 1, // Wajib menyertakan harga dalam kurs USD
+       'quantity' => 1,
+       'description' => 'Amazing Shoes'
+   ]
+], 'akunpaypalku@domain.com');
+
+/*
+ * --------------------------------------------------------------
+ * Melakukan transaksi pembayaran dengan multi produk
+ * 
+ * @return array|bool   Returns FALSE if failed or returns array contains
+ *                      Ipaymu transaction Url.
+ * --------------------------------------------------------------
+ */
+ $ipaymu->addTransaction([
+      'id' => 'INV-1234567890',
+      'products' => [
+          [
+              'name' => 'Shoes',
+              'price' => 10000,
+              'quantity' => 1,
+              'description' => 'Amazing Shoes'
+          ],
+          [
+              'name' => 'Bag',
+              'price' => 5000,
+              'quantity' => 2,
+              'description' => 'Amazing Bag'
+          ]
+      ]
+ ]);
+ 
+ /*
+  * --------------------------------------------------------------
+  * Melakukan transaksi pembayaran PayPal dengan multi produk
+  * 
+  * @return array|bool   Returns FALSE if failed or returns array contains
+  *                      Ipaymu transaction Url.
+  * --------------------------------------------------------------
+  */
+  $ipaymu->addTransaction([
+       'id' => 'INV-1234567890',
+       'products' => [
+           [
+               'name' => 'Shoes',
+               'price' => 10000,
+               'price_usd' => 1, // Wajib menyertakan harga dalam kurs USD
+               'quantity' => 1,
+               'description' => 'Amazing Shoes'
+           ],
+           [
+               'name' => 'Bag',
+               'price' => 5000,
+               'price_usd' => 1, // Wajib menyertakan harga dalam kurs USD
+               'quantity' => 2,
+               'description' => 'Amazing Bag'
+           ]
+       ]
+  ], 'akunpaypalku@domain.com');
+
+/*
+ * --------------------------------------------------------------
+ * Mendapatkan original response object.
+ * --------------------------------------------------------------
+ */
+ $response = $ipaymu->getResponse();
+ 
+/*
+ * --------------------------------------------------------------
+ * Mendapatkan informasi error.
+ * --------------------------------------------------------------
+ */
+ $errors = $ipaymu->getErrors();
+```
+
+Untuk keterangan lebih lengkap dapat dibaca di [Wiki](https://github.com/steevenz/ipaymu/wiki)
+
+Ide, Kritik dan Saran
+---------------------
+Jika anda memiliki ide, kritik ataupun saran, anda dapat mengirimkan email ke [steevenz@stevenz.com][3]. 
+Anda juga dapat mengunjungi situs pribadi saya di [steevenz.com][1]
+
+Bugs and Issues
+---------------
+Jika anda menemukan bugs atau issue, anda dapat mempostingnya di [Github Issues][6].
+
+Requirements
+------------
+- PHP 5.6+
+- [Composer][9]
+- [O2System Curl][10]
+
+Referensi
+---------
+Untuk mengetahui lebih lanjut mengenai RajaOngkir API, lihat di [Dokumentasi API Ipaymu][12].
+
+[1]: http://steevenz.com
+[2]: http://steevenz.com/blog/ipaymu-api
+[3]: mailto:steevenz@steevenz.com
+[4]: http://github.com/steevenz/ipaymu
+[5]: http://github.com/steevenz/ipaymu/wiki
+[6]: http://github.com/steevenz/ipaymu/issues
+[7]: https://packagist.org/packages/steevenz/ipaymu
+[9]: https://getcomposer.org
+[10]: http://github.com/o2system/curl
+[11]: http://ipaymu.com
+[12]: http://ipaymu.com/dokumentasi-api
---
+++ src/Ipaymu.php
@@ -0,0 +1,424 @@
+<?php
+/**
+ * Ipaymu API PHP Class Library
+ *
+ * Copyright (C) 2018  Steeve Andrian Salim (steevenz)
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * @author         Steeve Andrian Salim
+ * @copyright      Copyright (c) 2018, Steeve Andrian Salim
+ * @since          Version 2.0.0
+ * @filesource
+ */
+
+// ------------------------------------------------------------------------
+
+namespace Steevenz;
+
+// ------------------------------------------------------------------------
+
+use O2System\Curl;
+use O2System\Kernel\Http\Message\Uri;
+use O2System\Spl\Traits\Collectors\ConfigCollectorTrait;
+
+/**
+ * Class Ipaymu
+ * @package Steevenz
+ */
+class Ipaymu
+{
+    use ConfigCollectorTrait;
+
+    /**
+     * Ipaymu::$response
+     *
+     * Curl Response Object.
+     *
+     * @var \O2System\Curl\Response
+     */
+    protected $response;
+
+    /**
+     * Ipaymu::$errors
+     *
+     * Curl response errors.
+     *
+     * @access  protected
+     * @type    array
+     */
+    protected $errors = [];
+
+    // ------------------------------------------------------------------------
+
+    /**
+     * Ipaymu::__construct
+     *
+     * @param array $config
+     */
+    public function __construct(array $config = [])
+    {
+        if(count($config)) {
+            $this->setConfig($config);
+        }
+    }
+
+    // ------------------------------------------------------------------------
+
+    /**
+     * Ipaymu::setKey
+     *
+     * Sets Ipaymu API key.
+     *
+     * @param string $apiKey Ipaymu API key.
+     *
+     * @return static
+     */
+    public function setApiKey($apiKey)
+    {
+        $this->setConfig('apiKey', $apiKey);
+
+        return $this;
+    }
+
+    // ------------------------------------------------------------------------
+
+    /**
+     * Ipaymu::request
+     *
+     * Curl request API caller.
+     *
+     * @param string $path
+     * @param array  $params
+     * @param string $type
+     *
+     * @access  protected
+     * @return  array|bool Returns FALSE if failed.
+     */
+    protected function request($path, $params = [], $type = 'GET')
+    {
+        $apiUrl = 'https://my.ipaymu.com';
+        $path = 'api/' . $path;
+
+        $uri = (new Uri($apiUrl))->withPath($path);
+        $request = new Curl\Request();
+
+        $params[ 'key' ] = $this->config['apiKey'];
+        $params[ 'format' ] = 'json';
+
+        switch ($type) {
+            default:
+            case 'GET':
+                $this->response = $request->setUri($uri)->get($params);
+                break;
+
+            case 'POST':
+                $request->addHeader('content-type', 'application/x-www-form-urlencoded');
+                $this->response = $request->setUri($uri)->post($params);
+                break;
+        }
+
+        // Try to get curl error
+        if (false !== ($error = $this->response->getError())) {
+            $this->errors = $error;
+        } else {
+            $body = $this->response->getBody();
+
+            if (empty($body[ 'Status' ])) {
+                return $body->getArrayCopy();
+            } else {
+                $this->errors[ $body[ 'Status' ] ] = $body[ 'Keterangan' ];
+            }
+        }
+
+        return false;
+    }
+
+    // ------------------------------------------------------------------------
+
+    /**
+     * Ipaymu::getAccount
+     *
+     * Gets ipaymu account information.
+     *
+     * @return array|bool Returns FALSE if failed.
+     */
+    public function getAccount()
+    {
+        if (false !== ($response = $this->request('CekSaldo.php'))) {
+            $account = [
+                'username' => $response['Username'],
+                'balance' => $response['Saldo'],
+                'status' => $this->checkAccountStatus($response['Username'])
+            ];
+
+            return $account;
+        }
+
+        return false;
+    }
+
+    // ------------------------------------------------------------------------
+
+    /**
+     * Ipaymu::checkAccountBalance
+     *
+     * Gets latest ipaymu balance.
+     *
+     * @return int|bool Returns FALSE if failed.
+     */
+    public function checkAccountBalance()
+    {
+        if (false !== ($response = $this->request('CekSaldo.php'))) {
+            return (int) $response[ 'Saldo' ];
+        }
+
+        return false;
+    }
+
+    // ------------------------------------------------------------------------
+
+    /**
+     * Ipaymu::checkAccountStatus
+     *
+     * Gets ipaymu account status.
+     *
+     * @param string $username Ipaymu account username.
+     *
+     * @return string|bool Returns FALSE if failed.
+     */
+    public function checkAccountStatus($username)
+    {
+        $statusCodes = [
+            0 => 'UNVERIFIED',
+            1 => 'VERIFIED',
+            2 => 'CERTIFIED',
+            3 => 'CERTIFIED_PREMIUM',
+        ];
+
+        if (false !== ($response = $this->request('CekStatus.php', ['user' => $username]))) {
+            return $statusCodes[ $response[ 'StatusUser' ] ];
+        }
+
+        return false;
+    }
+
+    // ------------------------------------------------------------------------
+
+    /**
+     * Ipaymu::checkTransaction
+     *
+     * Gets ipaymu transaction status.
+     *
+     * @param string $trxId Transaction ID.
+     *
+     * @return array|bool Returns FALSE if failed.
+     */
+    public function checkTransaction($trxId)
+    {
+        $statusCodes = [
+            -1 => 'PROCESSED',
+            0  => 'PENDING',
+            1  => 'SUCCESS',
+            2  => 'CANCELED',
+            3  => 'REFUND',
+        ];
+
+        if (false !== ($response = $this->request('CekTransaksi.php', ['id' => $trxId]))) {
+
+            $response = [
+                'id'          => $trxId,
+                'status'      => $statusCodes[ $response[ 'Status' ] ],
+                'description' => $response[ 'Keterangan' ],
+                'sender'      => $response[ 'Pengirim' ],
+                'receiver'    => $response[ 'Penerima' ],
+                'amount'      => $response[ 'Nominal' ],
+                'time'        => date('r', strtotime($response[ 'Waktu' ])),
+                'type'        => strtoupper($response[ 'Tipe' ]),
+            ];
+
+            return $response;
+        }
+
+        return false;
+    }
+
+    // ------------------------------------------------------------------------
+
+    /**
+     * Ipaymu::addTransaction
+     *
+     * Add Ipaymu transaction.
+     *
+     * @example
+     * Single Product
+     * $ipaymu->addTransaction([
+     *      'id' => 'INV-1234567890',
+     *      'product' => [
+     *          'name' => 'Shoes'
+     *          'price' => 10000,
+     *          'quantity' => 1,
+     *          'description' => 'Amazing Shoes'
+     *      ]
+     * ]);
+     *
+     * Multiple Products
+     * $ipaymu->addTransaction([
+     *      'id' => 'INV-1234567890',
+     *      'products' => [
+     *          [
+     *              'name' => 'Shoes',
+     *              'price' => 10000,
+     *              'quantity' => 1,
+     *              'description' => 'Amazing Shoes'
+     *          ],
+     *          [
+     *              'name' => 'Bag',
+     *              'price' => 5000,
+     *              'quantity' => 2,
+     *              'description' => 'Amazing Bag'
+     *          ]
+     *      ]
+     * ]);
+     *
+     * Single Product with PayPal
+     * $ipaymu->addTransaction([
+     *      'id' => 'INV-1234567890',
+     *      'product' => [
+     *          'name' => 'Shoes'
+     *          'price' => 10000,
+     *          'price_usd' => 1, // Required for payment using PayPal
+     *          'quantity' => 1,
+     *          'description' => 'Amazing Shoes'
+     *      ]
+     * ], 'paypalemail@yourdomain.com');
+     *
+     * Multiple Products
+     * $ipaymu->addTransaction([
+     *      'id' => 'INV-1234567890',
+     *      'products' => [
+     *          [
+     *              'name' => 'Shoes',
+     *              'price' => 10000,
+     *              'price_usd' => 1, // Required for payment using PayPal
+     *              'quantity' => 1,
+     *              'description' => 'Amazing Shoes'
+     *          ],
+     *          [
+     *              'name' => 'Bag',
+     *              'price' => 5000,
+     *              'price_usd' => 1, // Required for payment using PayPal
+     *              'quantity' => 2,
+     *              'description' => 'Amazing Bag'
+     *          ]
+     *      ]
+     * ], 'paypalemail@yourdomain.com');
+     *
+     * @param array         $transaction    Transaction parameters, please see the example.
+     * @param string|null   $paypalAccount  Your PayPal email account.
+     *
+     * @return array|bool
+     */
+    public function addTransaction( array $transaction, $paypalAccount = null)
+    {
+        $params = [
+            'action'         => 'payment',
+            'invoice_number' => $transaction[ 'id' ],
+        ];
+
+        if (isset($transaction[ 'product' ])) {
+            $params[ 'product' ] = $transaction[ 'product' ][ 'name' ];
+            $params[ 'price' ] = $transaction[ 'product' ][ 'price' ];
+            $params[ 'quantity' ] = $transaction[ 'product' ][ 'quantity' ];
+
+            if (isset($transaction[ 'product' ][ 'description' ])) {
+                $params[ 'comments' ] = $transaction[ 'product' ][ 'description' ];
+            }
+
+            if(isset($paypalAccount)) {
+                $params['paypal_price'] = $transaction['product']['price_usd'];
+            }
+        } elseif (isset($transaction[ 'products' ])) {
+            $index = 0;
+            foreach ($transaction[ 'products' ] as $product) {
+                $params[ 'product' ][ $index ] = $product[ 'name' ];
+                $params[ 'price' ][ $index ] = $product[ 'price' ];
+                $params[ 'quantity' ][ $index ] = $product[ 'quantity' ];
+                $params[ 'comments' ][ $index ] = empty($product[ 'description' ]) ? null : $product[ 'description' ];
+
+                if(isset($options['paypal'])) {
+                    $params['paypal_price'][$index] = $product['price_usd'];
+                }
+            }
+        }
+
+        if(isset($this->config['url']['return'])) {
+            $params['ureturn'] = $this->config['url']['return'];
+        }
+
+        if(isset($this->config['url']['notify'])) {
+            $params['unotify'] = $this->config['url']['notify'];
+        }
+
+        if(isset($this->config['url']['cancel'])) {
+            $params['ucancel'] = $this->config['url']['cancel'];
+        }
+
+        if(isset($paypalAccount)) {
+            $params['paypal_email'] = $paypalAccount;
+        }
+
+        if (false !== ($response = $this->request('payment.htm'))) {
+            return $response;
+        }
+
+        return false;
+    }
+
+    // ------------------------------------------------------------------------
+
+    /**
+     * Ipaymu::getResponse
+     *
+     * Get original response object.
+     *
+     * @param   string $offset Response Offset Object
+     *
+     * @access  public
+     * @return  \O2System\Curl\Response|bool Returns FALSE if failed.
+     */
+    public function getResponse()
+    {
+        return $this->response;
+    }
+
+    // ------------------------------------------------------------------------
+
+    /**
+     * Ipaymu::getErrors
+     *
+     * Get errors request.
+     *
+     * @access  public
+     * @return  array|bool Returns FALSE if there is no errors.
+     */
+    public function getErrors()
+    {
+        if (count($this->errors)) {
+            return $this->errors;
+        }
+
+        return false;
+    }
+}
